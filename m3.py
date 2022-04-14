3aa67ff9dca0512f0e3e2315a0ad8208840cce5f
/home/student/.gitconfig <jenasr@csu.fullerton.edu>
2022-04-14 14:04:53 -0700

    post game, top wins

--- m3.py
+++ m3.py
@@ -33,10 +33,13 @@ class Result(BaseModel):
     timestamp: str
     number_of_guesses: int
 
+class TopWinner(BaseModel):
+    username: str
+    wins: int
 
 def get_db():
     """Connect words.db"""
-    with contextlib.closing(sqlite3.connect("stats.db", check_same_thread=False)) as db:
+    with contextlib.closing(sqlite3.connect("DB/stats.db", check_same_thread=False)) as db:
         db.row_factory = sqlite3.Row
         yield db
 
@@ -44,25 +47,54 @@ def get_db():
 app = FastAPI()
 
 
-@app.post("")
-async def add_game_played(result: Result, db: sqlite3.Connection = Depends(get_db)):
+@app.post("/stats/games/{game_id}")
+async def add_game_played(user_id: int, result: Result, db: sqlite3.Connection = Depends(get_db)):
     """Posting a win or loss"""
     # post into games
-    pass
+    cur = db.execute("SELECT user_id FROM users JOIN games USING user_id WHERE user_id = ?", [user_id])
+    looking_for = cur.fetchall()
+    if not looking_for:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
+        )
+    cur = db.execute("SELECT game_id, user_id FROM games WHERE game_id = ? AND user_id = ?", [game_id, user_id])
+    looking_for = cur.fetchall()
+    if not looking_for:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Game for player not found"
+        )
+    cur = db.execute("INSERT INTO games VALUES(?,?,?,?)", [user_id, game_id, result.timestamp, result.guesses, result.status])
+    db.commit()
+    return result
 
-@app.get("")
+@app.get("/stats/games/{user_id}")
 async def retrieve_player_stats(result: Result, db: sqlite3.Connection = Depends(get_db)):
     """Getting stats of a user"""
     # use table: games
+    cur = db.execute("SELECT user_id FROM users JOIN games USING user_id WHERE user_id = ?", [user_id])
+    looking_for = cur.fetchall()
+    if not looking_for:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
+        )
     pass
 
-@app.get("")
-async def retrieve_top_wins(result: Result, db: sqlite3.Connection = Depends(get_db)):
+
+@app.get("/stats/wins")
+async def retrieve_top_wins(db: sqlite3.Connection = Depends(get_db)):
     """Getting the top 10 users by number of wins"""
     # use view: wins
-    pass
+    cur = db.execute("SELECT username FROM users JOIN wins USING user_id LIMIT 10")
+    looking_for = cur.fetchall()
+    plr: TopWinner
+    plr_list = []
+    for i in range(len(looking_for)):
+        plr.username = looking_for[i][0]
+        plr.wins= looking_for[i][1]
+        plr_list.append(plr)
+    return {"top_wins": plr_list}
 
-@app.get("")
+@app.get("/stats/streaks")
 async def retrieve_top_streaks(result: Result, db: sqlite3.Connection = Depends(get_db)):
     """Getting the top 10 users by streak"""
     # use view: streaks
